1f6ef001f2477481141b29e14540425c9f3b3338
Prelang Builder <builder@prelang.com>
2016-01-05 00:27:15 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Goal' to allow for create, update

--- app/admin/goal.rb
+++ app/admin/goal.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Goal do
+  permit_params :name, :description, :repeats, :interval, :interval_unit
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
