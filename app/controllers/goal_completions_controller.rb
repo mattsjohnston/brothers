b6eae5ef098bb4d4b02cfbc117d198af55e5c8d0
Prelang Builder <builder@prelang.com>
2016-01-05 00:26:57 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/goal_completions_controller.rb
+++ app/controllers/goal_completions_controller.rb
@@ -1,4 +1,8 @@
 class GoalCompletionsController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_goal_completion, only: [:show, :edit, :update, :destroy]
 
   # GET /goal_completions
