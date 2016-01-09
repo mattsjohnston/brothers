23f2b8f9fce136963c79475d599abcaf39cb4207
Matt Johnston <matt@tallpixels.com>
2016-01-09 15:15:13 -0800

    Got rid of program migration

--- db/migrate/20160105084306_remove_programs.rb
+++
@@ -1,8 +0,0 @@
-class RemovePrograms < ActiveRecord::Migration
-  def change
-    remove_column :task_completions, :program_id, :integer
-    add_column :task_completions, :group_id, :integer
-    
-    drop_table :programs
-  end
-end
